1dc02e9471d3ab7538bf3954cff88a639828e70a
ZackaryOX <zekokhamis@gmail.com>
2019-09-23 17:29:21 -0400

    Prototype v2.1

---
+++ DMS Unity Game/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody player;
+    public float moveSpeed = 5;
+    private Vector3 moveDirection;
+    public bool CanMove = true;
+
+    Vector2 rotation = new Vector2(0, 0);
+    public float sensitivity = 1.5f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (CanMove == true)
+        {
+            rotation.y += Input.GetAxis("Mouse X");
+            rotation.x += -Input.GetAxis("Mouse Y");
+            player.transform.eulerAngles = (Vector2)rotation * sensitivity;
+
+            if (Input.GetKey("w"))
+            {
+                moveDirection = player.transform.rotation * Vector3.forward;
+            }
+            if (Input.GetKey("s"))
+            {
+                moveDirection = player.transform.rotation * -Vector3.forward;
+            }
+            if (Input.GetKey("d"))
+            {
+                moveDirection = player.transform.rotation * Vector3.right;
+            }
+            if (Input.GetKey("a"))
+            {
+                moveDirection = player.transform.rotation * -Vector3.right;
+            }
+
+            GetComponent<Rigidbody>().velocity = moveDirection * moveSpeed;
+            moveDirection = new Vector3(0, 0, 0);
+            player.transform.position = new Vector3(player.position.x, 3.0f, player.position.z);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ DMS Unity Game/Assets/Script/jumpScare.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpScare : MonoBehaviour
+{
+ 
+    bool mouseOver = false;
+    bool switchBool = false;
+    public PlayerMovement PMove;
+    public lightFlicker LFlick;
+    public GameObject lookingAt;
+    public GameObject flashLight;
+    public GameObject audio;
+    public AudioSource audio2;
+    public Material ghostmaterial;
+    public Material mapMaterial;
+    public Shader TVshader;
+    public GameObject Ghost;
+    public GameObject Player;
+    void Awake()
+    {
+
+        TVshader = Shader.Find("Custom/TVFlicker");
+
+        // rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (mouseOver == true)
+        {
+            flashLight.SetActive(false);
+            
+            if (switchBool == false)
+            {
+                Ghost.transform.position = new Vector3(6.0f,0.5f,24.2f);
+                Ghost.transform.rotation = new Quaternion(0.0f, -200.6f, 0.0f, 1);
+                Player.transform.position = new Vector3(4.6f, 2.0f, 22.304f);
+                Player.transform.Rotate(-Player.transform.rotation.eulerAngles);
+                Player.transform.Rotate(0,-67.0f, 0);
+                audio.GetComponent<AudioSource>().Play();
+                switchBool = true;
+                mapMaterial.shader = TVshader;
+                Shader standard = Shader.Find("Standard");
+                ghostmaterial.shader = standard;
+                PMove.CanMove = false;
+                PMove.moveSpeed = 0;
+                LFlick.StopFlicker = true;
+                audio2.PlayDelayed(1);
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        lookingAt = GameObject.Find(rayFromCamera.lookingAt);
+        mouseOver = true;
+        //if (mouseOver == true && switchBool == false)
+        //{
+        //GetComponent<Renderer>().material.SetVector("_Color2", newColor);
+        //switchBool = false;
+        //}else{
+        //    switchBool = true;
+        //}
+
+    }
+
+
+
+    void OnMouseExit()
+    {
+        mouseOver = false;
+        //if (mouseOver == false && switchBool == true)
+        //{
+        
+        //    switchBool = true;
+        //}else{
+        //    switchBool = false;
+        //}
+
+    }
+    void OnDestroy()
+    {
+        Shader standard = Shader.Find("Standard");
+        switchBool = false;
+        mapMaterial.shader = standard;
+        ghostmaterial.shader = Shader.Find("Outlined/Silhouetted Diffuse");
+        Ghost.transform.position = new Vector3(17.7f, 0.4f, -0.7f);
+        Ghost.transform.rotation = new Quaternion(0.0f, -66.8f, 0, 1);
+    }
+}
