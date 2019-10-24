5b8c67c8f89b2e75779f31b48bdea3f57d58c610
ZackaryOX <zekokhamis@gmail.com>
2019-10-24 18:53:11 -0400

    v2.2 Merged Vics Code
    Merged Victors Code

---
+++ DMS Unity Game/Assets/Script/EntityClass/Interactable/PickUp/PickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PickUp : Interactable
+{
+    //Constructor:
+    public PickUp(GameObject thisobject) : base(thisobject)
+    {
+
+    }
+
+    //Public:
+    public Image GetIcon()
+    {
+        return this.Icon;
+    } 
+
+    void SetImage(Image temp)
+    {
+        this.Icon = temp;
+    }
+    //Private:
+    private Image Icon;
+}
---
+++ DMS Unity Game/Assets/Script/EntityClass/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    //Constructor
+    public Player(GameObject thisobject) : base(thisobject)
+    {
+        ThisInput = new PlayerInput(thisobject);
+    }
+
+
+    //Public
+    public override void Update()
+    {
+        ThisInput.Update();
+    }
+
+    //Private
+    private PlayerInput ThisInput;
+    private Inventory ThisInventory;
+}
