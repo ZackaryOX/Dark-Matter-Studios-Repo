da0c8f373e5efa9dc84d1424ee205033f682a2f9
ZackaryOX <zekokhamis@gmail.com>
2019-11-04 16:37:55 -0500

    v2.5

---
+++ DMS Unity Game/Assets/Script/PlayerDependencies/Stamina.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamina
+{
+    //CONSTRUCTOR:
+    public Stamina(int stamina, float regen)
+    {
+        MaxStam = stamina;
+    }
+    //PUBLIC:
+
+    //PRIVATE:
+
+    private int MaxStam;
+    private int CurrentStam;
+    private int Regen;
+}
